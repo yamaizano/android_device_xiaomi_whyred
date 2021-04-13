ef5af5271c78063014a5bb2fad60ea12032f2cd4
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    whyred: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- device.mk
+++ device.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
