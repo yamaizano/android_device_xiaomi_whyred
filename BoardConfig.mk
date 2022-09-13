84ea69abbc2d3523a189b78a05c82f0928d2d9b8
Michael Bestas <mkbestas@gmail.com>
2022-09-13 17:21:38 -0300

    whyred: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,8 +177,8 @@ VENDOR_SECURITY_PATCH := 2018-11-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
