adae0303edaa02ad7d9960d28f1c3ca35ff15f27
Jiakai Zhang <jiakaiz@google.com>
2024-01-22 18:35:55 +0000

    whyred: Force PRODUCT_ENABLE_UFFD_GC to true
    We are going to make a breaking change, which requires either the
    kernel version being available to the build system or
    PRODUCT_ENABLE_UFFD_GC is explicitly set.
    
    Change-Id: I875f581aa573bcd67cfad4a15573513c800bcb21

--- device.mk
+++ device.mk
@@ -455,6 +455,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# UFFD GC
+PRODUCT_ENABLE_UFFD_GC := true
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
