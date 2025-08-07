99c29de8aba5ce7d5e09e2d597dda1743f842efc
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-08-07 19:23:58 +0700

    whyred: Import libinit from lineage xiaomi sdm660 tree
    also adapt to whyred
    
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_whyred
+TARGET_RECOVERY_DEVICE_MODULES := libinit_whyred
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
