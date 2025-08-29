7d53bce2564aa9957f834b289ea8c4a036cbecd8
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-08-29 17:39:33 +0700

    whyred: Adapt to LMODroid
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/lmodroid_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    lmodroid_whyred-user \
+    lmodroid_whyred-userdebug \
+    lmodroid_whyred-eng
--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
